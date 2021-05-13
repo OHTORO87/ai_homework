fd8eda0778712c55368c856abc39b04c1edc000b
WOOSEOK OH <83565884+OHTORO87@users.noreply.github.com>
2021-05-13 11:14:01 +0900

    Update homework_01.py

--- homework_01.py
+++ homework_01.py
@@ -17,3 +17,27 @@ for i in aws.rows:
 
 exf.save('c:\\dd\\outitx.xlsx')
 exf.close()
+
+'''
+SQLite version 3.35.5 2021-04-19 18:32:05
+Enter ".help" for usage hints.
+Connected to a transient in-memory database.
+Use ".open FILENAME" to reopen on a persistent database.
+sqlite> .oepn 'c:\\dd\\abd'
+Error: unknown command or invalid arguments:  "oepn". Enter ".help" for help
+sqlite> .open 'c:\\dd\\adb'
+sqlite> .mo co
+sqlite> .he on
+sqlite> create table mt(mount, alt);
+sqlite> .ta
+city   mt     score
+sqlite> .import 'c:\\dd\\mt.txt' mt
+sqlite> select * from mt;
+mount  alt
+-----  ----
+설악산    1900
+지로산    2915
+덕유산    1300
+sqlite>
+
+'''
